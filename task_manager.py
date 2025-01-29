577e7886662272e3077056ff13c10fb4432d1fd3
AnatoliiShara <Anatolii.Shara@ligazakon.ua>
2025-01-29 17:27:21 +0200

    Configuring main

---
+++ task_manager.py
@@ -0,0 +1,39 @@
+class Task:
+    
+    def __init__(self, title, description, status="new"):
+        self.title = title
+        self.description = description
+        self.status = status
+        
+    def __str__(self):
+        return f"{self.title} - {self.status}\n{self.description}"
+    
+class TaskManager:
+    
+    def __init__(self):
+        self.tasks = []
+        
+    def add_task(self, title, description):
+        task = Task(title, description)
+        self.tasks.append(task)
+        return len(self.tasks) - 1
+    
+    def get_task(self, index):
+        if 0 <= index < len(self.tasks):
+            return self.tasks[index]
+        return None
+    
+    def update_status(self, index, new_status):
+        if 0 <= index < len(self.tasks):
+            self.tasks[index].status = new_status
+            return True
+        return False
+    
+    def list_tasks(self):
+        return [str(task) for task in self.tasks]
+    
+
+if __name__ == "__main__":
+    manager = TaskManager()
+    task_id = manager.add_task("Test task", "This is a test task")
+  

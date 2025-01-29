0439ae02ca9901a75640da702b3f261606d0a28f
AnatoliiShara <Anatolii.Shara@ligazakon.ua>
2025-01-29 17:26:15 +0200

    Adding main

--- task_manager.py
+++ task_manager.py
@@ -32,3 +32,12 @@ class TaskManager:
     def list_tasks(self):
         return [str(task) for task in self.tasks]
     
+
+if __name__ == "__main__":
+    manager = TaskManager()
+    task_id = manager.add_task("Test task", "This is a test task")
+    print(manager.list_tasks())
+    task = manager.get_task(task_id)
+    print(task)
+    manager.update_status(task_id, "in progress")
+    print(manager.list_tasks())
